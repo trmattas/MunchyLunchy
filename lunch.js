7dcfdf215a3c3bdbe2a44ce9701804910f704279
trmattas <travis.mattas@gmail.com>
2016-07-11 11:12:28 -0700

    Added .html, .js, and .css files for the webpage

---
+++ lunch.js
@@ -0,0 +1,7 @@
+var restaurant = '';
+
+//it('should check ng-click', function() {
+//    expect(element(by.binding('restaurant')).getText()).toMatch('0');
+//    element(by.css('button')).click();
+//    expect(element(by.binding('restaurant')).getText()).toMatch('1');
+//});
